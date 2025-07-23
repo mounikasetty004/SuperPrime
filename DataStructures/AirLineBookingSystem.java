bcd9cd708f0ff4b7685c5eed7b9c10f419e00238
MounikaSetty <neelisettymounika982@gmail.com>
2025-07-23 17:40:26 +0530

    Update AirLineBookingSystem.java

--- DataStructures/AirLineBookingSystem.java
+++ DataStructures/AirLineBookingSystem.java
@@ -1,29 +1,34 @@
-
-import java.util.LinkedList;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class AirLineBookingSystem{
+public class AirLineBookingSystem {
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        ArrayList<String> namesList = new ArrayList<String>();
-        for(int i=0;i<n;i++){
-            String name=sc.next();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        ArrayList<String> namesList = new ArrayList<String>();  
+        for (int i = 0; i < n; i++) {
+            String name = sc.next();
             namesList.add(name);
         }
-         ArrayList<Integer> seatNo = new  ArrayList<Integer>();
-        for(int i=0;i<n;i++){
-            int No=sc.nextInt();
+
+        ArrayList<Integer> seatNo = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            int No = sc.nextInt();
             seatNo.add(No);
         }
-         ArrayList<String> cityList = new  ArrayList<String>();
-        for(int i=0;i<n;i++){
-            String =sc.next();
+
+        ArrayList<String> cityList = new ArrayList<String>();
+        for (int i = 0; i < n; i++) {
+            String city = sc.next();
             cityList.add(city);
         }
-        for(int i=0;i<n;i++){
-            System.out.println("Name: "+namesList[i]+"Seat no: "+seatNo[i]+"Destination: "+cityList[i]);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Name: " + namesList.get(i)  + " Seat no: " + seatNo.get(i) + " Destination: " + cityList.get(i));
         }
-    }
+
+        sc.close();
+    }        
 }
