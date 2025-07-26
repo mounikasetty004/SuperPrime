d8c183c388f1662f3214a4bc64c3de3027a4d9a4
MounikaSetty <neelisettymounika982@gmail.com>
2025-07-26 14:38:07 +0530

    Update LinkedTree.java

--- BinaryTree/LinkedTree.java
+++ BinaryTree/LinkedTree.java
@@ -1,5 +1,5 @@
 
-import java.util.Arrays;
+import java.util.*;
 
 class Node {
 
@@ -15,21 +15,33 @@ class Node {
 public class LinkedTree {
 
     Node root;
-    int c = 0;
-    char[] leaf = new char[10];
+    int c = 0, leafIndex = 0;
+    char[] leaf;
+
+    public LinkedTree(int size) {
+
+        leaf = new char[size];
+    }
 
     public void inorder(Node node) {
         if (node != null) {
             inorder(node.left);
             System.out.print(node.data + " ");
             c++;
-            inorder(node.right);
-            for (int i = 0; i < leaf.length; i++) {
-                if (node.left == null && node.right == null) {
-                    leaf[i] = node.data;
-                }
+            if (node.left == null && node.right == null && leafIndex < leaf.length) {
+                leaf[leafIndex++] = node.data;
             }
+            inorder(node.right);
         }
+    }
+
+    public int height(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+        return Math.max(leftHeight, rightHeight) + 1;
     }
 
     public void preorder(Node node) {
@@ -49,30 +61,34 @@ public class LinkedTree {
     }
 
     public static void main(String[] args) {
-        LinkedTree obj = new LinkedTree();
+
+        LinkedTree obj = new LinkedTree(10);
+
         obj.root = new Node('a');
         obj.root.left = new Node('b');
         obj.root.right = new Node('c');
         obj.root.left.left = new Node('d');
         obj.root.left.right = new Node('e');
         obj.root.left.left.right = new Node('g');
-
         obj.root.right.left = new Node('f');
         obj.root.left.right.left = new Node('h');
         obj.root.right.left.left = new Node('i');
         obj.root.right.left.right = new Node('j');
 
-        System.out.print("Inorder :");
+        System.out.print("Inorder : ");
         obj.inorder(obj.root);
         System.out.println();
-        System.out.println("inorder node counts:" + obj.c);
-        System.out.println("leaf nodes in inorder:" + Arrays.toString(obj.leaf));
-        System.out.print("preorder :");
+        System.out.println("Inorder node count: " + obj.c);
+        System.out.println("Leaf nodes in inorder: " + Arrays.toString(Arrays.copyOf(obj.leaf, obj.leafIndex)));
+
+        System.out.print("Preorder : ");
         obj.preorder(obj.root);
         System.out.println();
-        System.out.print("postorder :");
+
+        System.out.print("Postorder : ");
         obj.postorder(obj.root);
         System.out.println();
 
+        System.out.println("Height: " + obj.height(obj.root));
     }
 }
